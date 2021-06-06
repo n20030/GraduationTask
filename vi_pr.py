6bd33f3f814720825a964a1b94027ece0db43478
bob <n20030@std.it-college.ac.jp>
2021-06-07 00:26:58 +0900

    簡単な問題出力のプログラム

--- vi_pr.py
+++ vi_pr.py
@@ -1,10 +1,15 @@
-import random
+import random, time
 
-# TODO: （右、左、上、下）へ（数字）個移動してください。
-# TODO: 問題間に時間設定をする
-# TODO: 行頭や行末への移動の問題
 
-input_num = input('何問出しますか？')
-for i in range(int(input_num)):
-    random_num = random.randint(1, 100)
-    print('左へ ' + str(random_num) + ' 移動してください。')
+#（右、左、上、下）へ（数字）個移動してください。
+def problem_output():
+    input_num = input('何問出しますか？')
+    for i in range(int(input_num)):
+        random_num = random.randint(1, 100)
+        direction_list = ('左へ', '右へ', '上へ', '下へ')
+        direction = random.choice(direction_list)
+        print(direction + str(random_num) + ' 移動してください。')
+        time.sleep(5)
+
+problem_output()
+# 行頭や行末への移動の問題
